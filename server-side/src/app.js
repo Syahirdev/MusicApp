c5117b3fbafa7f64c8f5959ab047bf615371e574
Syahir <888syahir@gmail.com>
2020-08-11 07:03:34 +0800

    Revert "Revert "setting up registration routes""
    This reverts commit 8a00731ab6bc672c6fe9eea0835af4c61194286b.

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser'); //to process json data
+const bodyParser = require('body-parser'); //to process json data from AJAX request
 const cors = require('cors');
-const morgan = require('morgan');
+const morgan = require('morgan'); //console logging framework for route requests, for example it'll show type of request. to what route, status code, and time to respond
 
 const app = express();
 app.use(bodyParser.json()); //allow express app to parse any json request
 app.use(cors()); //any host/client to access server-side
-app.use(morgan('combined')); //log data from client
+app.use(morgan('tiny')); //log data from client (combined/tiny)
 
 //ROUTES
 app.get('/status', (req, res) => {
 	res.send({
 		message: 'Hello World! :D',
+	});
+});
+
+app.post('/register', (req, res) => {
+	res.send({
+		message: `Hello ${req.body.email}! You have successfully registered`,
 	});
 });
 
